e29234d3dd63ac6355be6ee016a0115c6bb211d9
cobusc <cobus.carstens@gmail.com>
2018-01-15 16:06:26 +0200

    Added first set of permission-related functions

---
+++ management_layer/permission/utils.py
@@ -0,0 +1,226 @@
+"""
+This module contains functions that are used to check whether certain calls
+are permitted.
+
+Permissions are based on the roles assigned to the user making the request,
+which maps to permissions on resources.
+"""
+import typing
+from pymemcache.client.base import PooledClient
+from management_layer.access_control.apis.access_control_api import \
+    AccessControlApi
+from management_layer.access_control.apis.operational_api import OperationalApi
+
+# Convenience types
+Uuid = typing.TypeVar("Uuid", str)
+User = typing.TypeVar("User", Uuid)
+Resource = typing.TypeVar("Resource", str)
+Permission = typing.TypeVar("Permission", str)
+ResourcePermissions = typing.List("ResourcePermissions",
+                                  typing.Tuple[Resource, Permission])
+Site = typing.TypeVar("Site", Uuid, None)
+Domain = typing.Union("Domain", str, int, None)
+Role = typing.TypeVar("Role", str, int, None)
+
+# Memcache key prefixes
+MKP_ROLE_RESOURCE_PERMISSION = "RRP"
+MKP_USER_ROLES = "UR"
+
+TECH_ADMIN = "tech_admin"
+MEMCACHE = PooledClient()
+CACHE_TIME = 5 * 60
+
+OA = OperationalApi()
+AA = AccessControlApi()
+
+# Name to id mappings. TODO: Populate via API on startup. Move somewhere
+# more appropriate.
+SITES = {}
+DOMAINS = {}
+ROLES = {}
+RESOURCES = {}
+
+
+def has_permissions(
+    user: User, operator: typing.Callable[[[bool]], bool],
+    resource_permissions: ResourcePermissions,
+    site: Site=None,
+    domain: Domain=None,
+    nocache: bool=False
+) -> bool:
+    """
+    Check whether the specified user has any or all (as per the operator) of
+    the specified resource permissions.
+    To establish whether this is the case, we need to interrogate the roles
+    associated with the user. If any of the roles has the required
+    permission, the requirement is satisfied.
+    :param user: The UUID of the user performing the action
+    :param operator: any or all
+    :param resource_permissions: The resource permissions required
+    :param site: The site from which the user is making the request
+    :param domain: The domain for which the user is making the request
+    :param nocache: Bypass the cache if True
+    :return: True if the user is has the required permissions on the resources
+    """
+    # Either a site or domain needs to be provided
+    if bool(site) == bool(domain):
+        raise RuntimeError("Either a site or a domain needs to be provided")
+
+    if operator not in [all, any]:
+        raise RuntimeError("The operator must be all or any")
+
+    roles = get_user_roles(user, site=site, domain=domain)
+    if TECH_ADMIN in roles:
+        return True
+
+    return operator(
+        # Any role can provide the permission.
+        any(has_permission(role, permission, resource, nocache) for role in
+            roles)
+        for permission, resource in resource_permissions
+    )
+
+
+def has_permission(
+    role: Role, permission: Permission, resource: Resource, nocache: bool=False
+) -> bool:
+    """
+    Check if a role has a specified resource permission.
+
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+
+    :param role: The role to check
+    :param permission: The required permission
+    :param resource: The resource
+    :param nocache: Bypass the cache if True
+    :return: True if the role has the permission on the resource, else False
+    """
+    result = False
+    permissions = get_role_resource_permissions(role, resource, nocache)
+    if permissions:
+        result = permission in permissions
+
+    return result
+
+
+def get_user_roles(
+    user: User, site: Site = None, domain: Domain = None, nocache: bool=False
+) -> typing.List[Role]:
+    """
+    Get the roles assigned to the user for the specified site or domain.
+    Either a site or a domain needs to be specified.
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+    :param user: The user
+    :param site: The site
+    :param domain: The domain
+    :param nocache: Bypass the cache if True
+    :return: A list of roles ids
+    """
+    # Either a site or domain needs to be provided
+    if bool(site) == bool(domain):
+        raise RuntimeError("Either a site or a domain needs to be provided")
+
+    if domain:
+        result = get_user_roles_for_domain(user, domain, nocache)
+    else:
+        result = get_user_roles_for_site(user, site, nocache)
+
+    return result
+
+
+def get_role_resource_permissions(
+    role: Role, resource: Resource, nocache: bool=False
+) -> typing.List[Permission]:
+    """
+    Get the permission granted on a resource for a specified role.
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+
+    :param role: The role
+    :param resource: The resource
+    :param nocache: Bypass the cache if True
+    :return: A list of permission ids
+    """
+    try:
+        role_id = int(role)
+    except TypeError:
+        role_id = ROLES[role]
+
+    try:
+        resource_id = int(resource)
+    except TypeError:
+        resource_id = RESOURCES[resource]
+
+    key = "{}:{}:{}".format(MKP_ROLE_RESOURCE_PERMISSION, role_id, resource_id)
+    role_resource_permissions = None if nocache else MEMCACHE.get(key)
+    if not role_resource_permissions:
+        role_resource_permissions = AA.roleresourcepermission_list(
+            role_id=role_id, resource_id=resource_id
+        )
+        MEMCACHE.set(key, role_resource_permissions, expire=CACHE_TIME)
+
+    return role_resource_permissions
+
+
+def api_get_user_roles(
+    user: User, nocache: bool=False
+) -> typing.Dict[str, typing.List[Role]]:
+    """
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+    :param user: The user for which to get roles
+    :param nocache: Bypass the cache if True
+    :return: A dictionary containing the roles assigned to the specified user on
+       the domains and sites in the system.
+    """
+    key = "{}:{}".format(MKP_USER_ROLES, user)
+    user_roles = None if nocache else MEMCACHE.get(key)
+    if not user_roles:
+        user_roles = OA.get_all_user_roles(user)
+        MEMCACHE.set(key, user_roles, expire=CACHE_TIME)
+
+    return user_roles
+
+
+def get_user_roles_for_domain(
+    user: User, domain: Domain, nocache: bool=False
+) -> typing.List[Role]:
+    """
+    Get the roles that a user has for a specified domain.
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+    :param user: The user
+    :param domain: The site
+    :param nocache: Bypass the cache if True
+    :return: A list of role ids
+    """
+    try:
+        domain_id = int(domain)
+    except TypeError:
+        domain_id = DOMAINS[domain]
+
+    user_roles = api_get_user_roles(user, nocache)
+    return user_roles.get("d:{}".format(domain_id), [])
+
+
+def get_user_roles_for_site(
+    user: User, site: Site, nocache: bool=False
+) -> typing.List[Role]:
+    """
+    Get the roles that a user has for a specified site.
+    Bypassing the cache can have a significant performance and should only be
+    used in exceptional circumstances.
+    :param user: The user
+    :param site: The site
+    :param nocache: Bypass the cache if True
+    :return: A list of role ids
+    """
+    try:
+        site_id = int(site)
+    except TypeError:
+        site_id = SITES[site]
+
+    user_roles = api_get_user_roles(user, nocache)
+    return user_roles.get("s:{}".format(site_id), [])
