796bc2a65aa3cf78eb681947cc53ff55b093e165
cobusc <cobus.carstens@gmail.com>
2018-01-22 14:42:04 +0200

    Generated server code using new version (2.3.1) of the Swagger Codegen tool

--- swagger_server/controllers/oidc_provider_controller.py
+++ swagger_server/controllers/oidc_provider_controller.py
@@ -1,16 +1,16 @@
 import connexion
-from swagger_server.models.client import Client
-from swagger_server.models.user import User
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
+import six
+
+from swagger_server.models.client import Client  # noqa: E501
+from swagger_server.models.user import User  # noqa: E501
+from swagger_server import util
 
 
-def client_list(offset=None, limit=None, client_ids=None):
-    """
-    client_list
-    
+def client_list(offset=None, limit=None, client_ids=None):  # noqa: E501
+    """client_list
+
+     # noqa: E501
+
     :param offset: An optional query parameter specifying the offset in the result set to start from.
     :type offset: int
     :param limit: An optional query parameter to limit the number of results returned.
@@ -23,34 +23,41 @@ def client_list(offset=None, limit=None, client_ids=None):
     return 'do some magic!'
 
 
-def client_read(client_id):
-    """
-    client_read
-    
+def client_read(client_id):  # noqa: E501
+    """client_read
+
+     # noqa: E501
+
     :param client_id: A UUID value identifying the client
-    :type client_id: str
+    :type client_id: dict | bytes
 
     :rtype: Client
     """
+    if connexion.request.is_json:
+        client_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def user_delete(user_id):
-    """
-    user_delete
-    
+def user_delete(user_id):  # noqa: E501
+    """user_delete
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def user_list(offset=None, limit=None, email=None, user_ids=None):
-    """
-    user_list
-    
+def user_list(offset=None, limit=None, email=None, user_ids=None):  # noqa: E501
+    """user_list
+
+     # noqa: E501
+
     :param offset: An optional query parameter specifying the offset in the result set to start from.
     :type offset: int
     :param limit: An optional query parameter to limit the number of results returned.
@@ -65,29 +72,35 @@ def user_list(offset=None, limit=None, email=None, user_ids=None):
     return 'do some magic!'
 
 
-def user_read(user_id):
-    """
-    user_read
-    
+def user_read(user_id):  # noqa: E501
+    """user_read
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
 
     :rtype: User
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def user_update(user_id, data=None):
-    """
-    user_update
-    
+def user_update(user_id, data=None):  # noqa: E501
+    """user_update
+
+     # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param data: 
     :type data: dict | bytes
 
     :rtype: None
     """
     if connexion.request.is_json:
-        data = User.from_dict(connexion.request.get_json())
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
+    if connexion.request.is_json:
+        data = User.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
