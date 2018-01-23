f00eec7d9949ebdd05bc719d3f70d05e1d64f2e8
cobusc <cobus.carstens@gmail.com>
2018-01-23 16:49:22 +0200

    Added generated client code

---
+++ management_layer/authentication_service/swagger_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.authentication_api import AuthenticationApi
+from swagger_client.api.experimental_api import ExperimentalApi
+from swagger_client.api.oidc_provider_api import OidcProviderApi
