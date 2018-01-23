9084727c79196b8b60c90e75092f2c6b90741d2b
cobusc <cobus.carstens@gmail.com>
2018-01-23 16:38:22 +0200

    Regenerated server code using updated swagger spec and new codegentool version

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,14 +1,14 @@
 import connexion
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
+import six
+
+from swagger_server import util
 
 
-def sites_site_id_activate_get(site_id):
-    """
-    sites_site_id_activate_get
-    Activate the site so that users can log in to it.
+def sites_site_id_activate_get(site_id):  # noqa: E501
+    """sites_site_id_activate_get
+
+    Activate the site so that users can log in to it. # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -17,10 +17,11 @@ def sites_site_id_activate_get(site_id):
     return 'do some magic!'
 
 
-def sites_site_id_deactivate_get(site_id):
-    """
-    sites_site_id_deactivate_get
-    Deactivate the site so that users cannot log in to it.
+def sites_site_id_deactivate_get(site_id):  # noqa: E501
+    """sites_site_id_deactivate_get
+
+    Deactivate the site so that users cannot log in to it. # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -29,25 +30,31 @@ def sites_site_id_deactivate_get(site_id):
     return 'do some magic!'
 
 
-def users_user_id_activate_get(user_id):
-    """
-    users_user_id_activate_get
-    Activate the user&#39;s account.
+def users_user_id_activate_get(user_id):  # noqa: E501
+    """users_user_id_activate_get
+
+    Activate the user&#39;s account. # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def users_user_id_deactivate_get(user_id):
-    """
-    users_user_id_deactivate_get
-    Deactivate the user&#39;s account.
+def users_user_id_deactivate_get(user_id):  # noqa: E501
+    """users_user_id_deactivate_get
+
+    Deactivate the user&#39;s account. # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
--- swagger_server/controllers/operational_controller.py
+++ swagger_server/controllers/operational_controller.py
@@ -1,31 +1,34 @@
 import connexion
-from swagger_server.models.all_user_roles import AllUserRoles
-from swagger_server.models.domain_roles import DomainRoles
-from swagger_server.models.site_and_domain_roles import SiteAndDomainRoles
-from swagger_server.models.site_role_labels_aggregated import SiteRoleLabelsAggregated
-from swagger_server.models.user_site_role_labels_aggregated import UserSiteRoleLabelsAggregated
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
+import six
+
+from swagger_server.models.all_user_roles import AllUserRoles  # noqa: E501
+from swagger_server.models.domain_roles import DomainRoles  # noqa: E501
+from swagger_server.models.site_and_domain_roles import SiteAndDomainRoles  # noqa: E501
+from swagger_server.models.site_role_labels_aggregated import SiteRoleLabelsAggregated  # noqa: E501
+from swagger_server.models.user_site_role_labels_aggregated import UserSiteRoleLabelsAggregated  # noqa: E501
+from swagger_server import util
 
 
-def get_all_user_roles(user_id):
-    """
-    get_all_user_roles
-    Get the effective roles that a user has at any place in the organisational tree.
+def get_all_user_roles(user_id):  # noqa: E501
+    """get_all_user_roles
+
+    Get the effective roles that a user has at any place in the organisational tree. # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
 
     :rtype: AllUserRoles
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def get_domain_roles(domain_id):
-    """
-    get_domain_roles
-    Get the domain and its lineage&#39;s roles defined for a domain.
+def get_domain_roles(domain_id):  # noqa: E501
+    """get_domain_roles
+
+    Get the domain and its lineage&#39;s roles defined for a domain. # noqa: E501
+
     :param domain_id: A unique integer value identifying the domain.
     :type domain_id: int
 
@@ -34,10 +37,11 @@ def get_domain_roles(domain_id):
     return 'do some magic!'
 
 
-def get_site_and_domain_roles(site_id):
-    """
-    get_site_and_domain_roles
-    Get the site- and domain lineage roles defined for a given site.
+def get_site_and_domain_roles(site_id):  # noqa: E501
+    """get_site_and_domain_roles
+
+    Get the site- and domain lineage roles defined for a given site. # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -46,10 +50,11 @@ def get_site_and_domain_roles(site_id):
     return 'do some magic!'
 
 
-def get_site_role_labels_aggregated(site_id):
-    """
-    get_site_role_labels_aggregated
-    Get a list of all possible role labels that a user can have from the specified site&#39;s perspective.
+def get_site_role_labels_aggregated(site_id):  # noqa: E501
+    """get_site_role_labels_aggregated
+
+    Get a list of all possible role labels that a user can have from the specified site&#39;s perspective. # noqa: E501
+
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
@@ -58,15 +63,18 @@ def get_site_role_labels_aggregated(site_id):
     return 'do some magic!'
 
 
-def get_user_site_role_labels_aggregated(user_id, site_id):
-    """
-    get_user_site_role_labels_aggregated
-    Get a list of all role labels that the specified user has from the specified site&#39;s perspective.
+def get_user_site_role_labels_aggregated(user_id, site_id):  # noqa: E501
+    """get_user_site_role_labels_aggregated
+
+    Get a list of all role labels that the specified user has from the specified site&#39;s perspective. # noqa: E501
+
     :param user_id: A UUID value identifying the user.
-    :type user_id: str
+    :type user_id: dict | bytes
     :param site_id: A unique integer value identifying the site.
     :type site_id: int
 
     :rtype: UserSiteRoleLabelsAggregated
     """
+    if connexion.request.is_json:
+        user_id = .from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
