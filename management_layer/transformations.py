bbfc2ccad9a7eef88294f884cb869baef4fe8d52
cobusc <cobus.carstens@gmail.com>
2018-02-19 10:40:24 +0200

    Additional transformations

--- management_layer/transformations.py
+++ management_layer/transformations.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from management_layer.transformation import Transformation, Mapping
 
@@ -7,12 +7,16 @@ def datetime_to_string(x: datetime) -> str:
     return x.strftime("%Y:%m:%dT%H:%M:%SZ")
 
 
-DOMAIN_ROLE = Transformation(
+def datetime_to_string(x: date) -> str:
+    return x.strftime("%Y:%m:%d")
+
+
+ADMIN_NOTE = Transformation(
     mappings=[
         Mapping("created_at", conversion=datetime_to_string),
         Mapping("updated_at", conversion=datetime_to_string)
     ],
-    copy_fields=["domain_id", "role_id", "grant_implicitly"]
+    copy_fields=["id", "user_id", "creator_id", "note"]
 )
 
 DOMAIN = Transformation(
@@ -23,4 +27,36 @@ DOMAIN = Transformation(
     copy_fields=["id", "parent_id", "name", "description"]
 )
 
+DOMAIN_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["domain_id", "role_id", "grant_implicitly"]
+)
+
+SITE_DATA_SCHEMA = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["site_id", "schema"]
+)
+
+USER_DOMAIN_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["user_id", "domain_id", "role_id"]
+)
+
+USER_SITE_DATA = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string),
+        Mapping("consented_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["user_id", "site_id", "data", "blocked"]
+)
 
