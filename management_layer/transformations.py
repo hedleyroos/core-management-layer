497187e0cce944218c6e654c76470364412d333e
cobusc <cobus.carstens@gmail.com>
2018-02-19 14:16:14 +0200

    More transformation definitions

--- management_layer/transformations.py
+++ management_layer/transformations.py
@@ -35,6 +35,73 @@ DOMAIN_ROLE = Transformation(
     copy_fields=["domain_id", "role_id", "grant_implicitly"]
 )
 
+INVITATION = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string),
+        Mapping("expires_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["id", "first_name", "last_name", "email",
+                 "invitor_id", "is_system_user"]
+)
+
+INVITATION_DOMAIN_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["invitation_id", "domain_id", "role_id"]
+)
+
+INVITATION_SITE_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["invitation_id", "site_id", "role_id"]
+)
+
+PERMISSION = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["id", "name", "description"]
+)
+
+RESOURCE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["id", "urn", "description"]
+)
+
+ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["id", "label", "description", "requires_2fa"]
+)
+
+ROLE_RESOURCE_PERMISSION = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["role_id", "resource_id", "permission_id"]
+)
+
+SITE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["id", "name", "domain_id", "description",
+                 "client_id", "is_active"]
+)
+
 SITE_DATA_SCHEMA = Transformation(
     mappings=[
         Mapping("created_at", conversion=datetime_to_string),
@@ -43,12 +110,28 @@ SITE_DATA_SCHEMA = Transformation(
     copy_fields=["site_id", "schema"]
 )
 
+SITE_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["site_id", "role_id", "grant_implicitly"]
+)
+
 USER_DOMAIN_ROLE = Transformation(
     mappings=[
         Mapping("created_at", conversion=datetime_to_string),
         Mapping("updated_at", conversion=datetime_to_string)
     ],
     copy_fields=["user_id", "domain_id", "role_id"]
+)
+
+USER_SITE_ROLE = Transformation(
+    mappings=[
+        Mapping("created_at", conversion=datetime_to_string),
+        Mapping("updated_at", conversion=datetime_to_string)
+    ],
+    copy_fields=["user_id", "site_id", "role_id"]
 )
 
 USER_SITE_DATA = Transformation(
