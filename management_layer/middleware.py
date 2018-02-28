63f8590932ed588d13da7fc09c6b2e2db262769e
cobusc <cobus.carstens@gmail.com>
2018-02-28 14:23:29 +0200

    Added explicit error messages for different token validation scenarios

--- management_layer/middleware.py
+++ management_layer/middleware.py
@@ -6,6 +6,9 @@ from management_layer.settings import JWT_SECRET, JWT_ALGORITHM, AUDIENCE, INSEC
 
 LOGGER = logging.getLogger(__name__)
 
+INVALID_TOKEN_STATUS = 401
+MISSING_TOKEN_STATUS = 400
+
 
 async def auth_middleware(app, handler):
     async def middleware(request):
@@ -13,25 +16,40 @@ async def auth_middleware(app, handler):
         if jwt_token:
             jwt_token = jwt_token.split(" ")[1]  # Strip the "bearer " prefix
             LOGGER.debug("JWT Token: {}".format(jwt_token))
+            LOGGER.debug(jwt.get_unverified_header(jwt_token))
             try:
                 payload = jwt.decode(jwt_token, JWT_SECRET,
                                      algorithms=[JWT_ALGORITHM],
                                      audience=AUDIENCE)
                 LOGGER.debug("Token payload: {}".format(payload))
-            except (jwt.DecodeError, jwt.ExpiredSignatureError):
-                return json_response({"message": "Token is invalid"}, status=400)
+            except jwt.DecodeError:
+                return json_response({"message": "Token is invalid"},
+                                     status=INVALID_TOKEN_STATUS)
+            except jwt.ExpiredSignatureError:
+                return json_response({"message": "Token expired"},
+                                     status=INVALID_TOKEN_STATUS)
+            except jwt.InvalidAudienceError:
+                return json_response({"message": "Token audience is invalid"},
+                                     status=INVALID_TOKEN_STATUS)
+            except jwt.InvalidIssuerError:
+                return json_response({"message": "Token issuer is invalid"},
+                                     status=INVALID_TOKEN_STATUS)
+            except Exception as e:
+                return json_response({"message": "Exception: {} {}".format(type(e), str(e))},
+                                     status=INVALID_TOKEN_STATUS)
 
             request["user_id"] = payload["sub"]
-            request["site_id"] = payload["aud"]
+            request["client_id"] = payload["aud"]
             request["token"] = payload
-            LOGGER.debug("")
+            LOGGER.debug("Token payload: {}".format(payload))
         elif INSECURE:
             request["user_id"] = "test_user"
-            request["site_id"] = "test_site"
+            request["client_id"] = "test_site"
             request["token"] = {}
             LOGGER.debug("Faking credentials because INSECURE is true.")
         else:
-            return json_response({"message": "Token is invalid"}, status=400)
+            return json_response({"message": "An authentication token is required"},
+                                 status=MISSING_TOKEN_STATUS)
 
         return await handler(request)
     return middleware
