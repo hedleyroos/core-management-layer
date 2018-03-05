e2a24339506d6002409863e36e8d7a43448d5881
cobusc <cobus.carstens@gmail.com>
2018-03-05 07:13:16 +0200

    Description typo fix

--- management_layer/api/stubs.py
+++ management_layer/api/stubs.py
@@ -1016,7 +1016,7 @@ class MockedStubClass(AbstractStubClass):
                 "type": "string"
             },
             "reuse_consent": {
-                "description": "If enabled, the Server will save the user consent given to a specific client, so that user wont be prompted for the same authorization multiple times.",
+                "description": "If enabled, the Server will save the user consent given to a specific client, so that user will not be prompted for the same authorization multiple times.",
                 "type": "boolean"
             },
             "terms_url": {
