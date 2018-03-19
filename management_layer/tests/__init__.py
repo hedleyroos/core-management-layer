f089cb64e22ffc603340e799ec04d4d348a62bef
AltusBarry <AltusBarry@users.noreply.github.com>
2018-03-19 15:16:18 +0200

    feature/add-environs: Altered env vars for tests

--- management_layer/tests/__init__.py
+++ management_layer/tests/__init__.py
@@ -1,8 +1,23 @@
 import os
 
 
-orig_environ = dict(os.environ)
-orig_environ["ACCESS_CONTROL_API"] = "localhost/"
-orig_environ["AUTHENTICATION_SERVICE_API"] = "localhost/"
-orig_environ["USER_DATA_STORE_API"] = "localhost/"
-os.environ.update(orig_environ)
+# Ensure only tests get altered env vars.
+if __name__ != "__main__":
+    print ("*"*20)
+    print ("Test __init__ fired:")
+    print (__name__)
+    print ("*"*20)
+
+    ACCESS_CONTROL_PORT = 60000
+    AUTHENTICATION_SERVICE_PORT = 60001
+    USER_DATA_STORE_PORT = 60002
+    orig_environ = dict(os.environ)
+
+    # No secrets get updated here. That is still left to each test to sort out.
+    orig_environ.update({
+        "STUBS_CLASS": "management_layer.integration.Implementation",
+        "ACCESS_CONTROL_API": "http://localhost:{}/api/v1".format(ACCESS_CONTROL_PORT),
+        "AUTHENTICATION_SERVICE_API": "http://localhost:{}/api/v1".format(AUTHENTICATION_SERVICE_PORT),
+        "USER_DATA_STORE_API": "http://localhost:{}/api/v1".format(USER_DATA_STORE_PORT),
+    })
+    os.environ.update(orig_environ)
